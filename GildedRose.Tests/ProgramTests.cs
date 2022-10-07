2649519e107200e9eab00a949d52e366ac7b624e
Lucasfth <luha@itu.dk>
2022-10-07 13:11:25 +0200

    add first test of normal object

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -3,8 +3,19 @@ namespace GildedRose.Tests;
 public class ProgramTests
 {
     [Fact]
-    public void TestTheTruth()
+    public void Normal_Item_Degrades_By_1_Quality_When_Not_Expired()
     {
-        true.Should().BeTrue();
+        // Arrange
+        var items = new Item { Name = "Normal Item", SellIn = 10, Quality = 20 };
+        var np = new Program();
+        np.Items = new List<Item> { items };
+
+        // Act
+        np.UpdateQuality();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Normal Item");
+        np.Items[0].SellIn.Should().Be(9);
+        np.Items[0].Quality.Should().Be(19);
     }
 }
