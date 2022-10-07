a741b25c07e50b648e844b227a4b0c6468240a2b
Frederik Petersen <fred.p@live.dk>
2022-10-07 13:35:08 +0200

    Normal item expired test

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -20,6 +20,23 @@ public class ProgramTests
     }
 
     [Fact]
+    public void Normal_Item_Degrades_By_2_Quality_When_Expired()
+    {
+        // Arrange
+        var items = new Item { Name = "Normal Item", SellIn = 0, Quality = 20 };
+        var np = new Program();
+        np.Items = new List<Item> { items };
+
+        // Act
+        np.UpdateQuality();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Normal Item");
+        np.Items[0].SellIn.Should().Be(-1);
+        np.Items[0].Quality.Should().Be(18);
+    }
+
+    [Fact]
     public void Aged_Brie_Increases_By_1_Quality_When_Not_Expired()
     {
         // Arrange
