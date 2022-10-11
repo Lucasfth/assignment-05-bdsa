7d9b6c2facaa92fa3590bef2ff6f81223abb1c67
Lucasfth <luha@itu.dk>
2022-10-11 12:54:31 +0200

    add tests for each item as conjured
    There may be some missing

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -20,6 +20,23 @@ public class ProgramTests
     }
 
     [Fact]
+    public void Normal_Conjured_Item_Degrades_By_2_Quality_When_Not_Expired()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Normal Item", 10, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Normal Item");
+        np.Items[0].SellIn.Should().Be(9);
+        np.Items[0].Quality.Should().Be(18);
+    }
+
+    [Fact]
     public void Normal_Item_Degrades_By_2_Quality_When_Expired()
     {
         // Arrange
@@ -34,6 +51,23 @@ public class ProgramTests
         np.Items[0].Name.Should().Be("Normal Item");
         np.Items[0].SellIn.Should().Be(-1);
         np.Items[0].Quality.Should().Be(18);
+    }
+
+    [Fact]
+    public void Normal_Conjured_Item_Degrades_By_4_Quality_When_Expired()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Normal Item", 0, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Normal Item");
+        np.Items[0].SellIn.Should().Be(-1);
+        np.Items[0].Quality.Should().Be(16);
     }
 
     [Fact]
@@ -54,6 +88,23 @@ public class ProgramTests
     }
 
     [Fact]
+    public void Aged_Conjured_Brie_Increases_By_2_Quality_When_Not_Expired()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Aged Brie", 10, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Aged Brie");
+        np.Items[0].SellIn.Should().Be(9);
+        np.Items[0].Quality.Should().Be(22);
+    }
+
+    [Fact]
     public void Aged_Brie_Increases_By_2_Quality_When_Expired()
     {
         // Arrange
@@ -71,7 +122,24 @@ public class ProgramTests
     }
 
     [Fact]
-    public void Legendary_Item_Degrades_only_by_SellIn()
+    public void Aged_Conjured_Brie_Increases_By_4_Quality_When_Expired()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Aged Brie", 0, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Aged Brie");
+        np.Items[0].SellIn.Should().Be(-1);
+        np.Items[0].Quality.Should().Be(24);
+    }
+
+    [Fact]
+    public void Legendary_Item_Does_Not_Change()
     {
         //Arrange
         var np = new Program();
@@ -85,7 +153,23 @@ public class ProgramTests
         np.Items[0].Name.Should().Be("Sulfuras, Hand of Ragnaros");
         np.Items[0].SellIn.Should().Be(0);
         np.Items[0].Quality.Should().Be(80);
+    }
 
+    [Fact]
+    public void Legendary_Conjured_Item_Does_Not_Change()
+    {
+        //Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Sulfuras, Hand of Ragnaros", 0, 80);
+        np.Items = new List<Item> { item };
+        
+        //Act
+        np.UpdateItems();
+        
+        //Assert
+        np.Items[0].Name.Should().Be("Conjured Sulfuras, Hand of Ragnaros");
+        np.Items[0].SellIn.Should().Be(0);
+        np.Items[0].Quality.Should().Be(80);
     }
 
     [Fact]
@@ -106,7 +190,7 @@ public class ProgramTests
     }
     
     [Fact]
-    public void Backstage_Pass_Increase_By_1_When_SellIn_More_Than_10()
+    public void Backstage_Pass_Increase_By_1_Quality_When_SellIn_More_Than_10()
     {
         // Arrange
         var np = new Program();
@@ -123,7 +207,24 @@ public class ProgramTests
     }
 
     [Fact]
-    public void Backstage_Pass_Increase_By_2_When_SellIn_Less_Than_10_Days()
+    public void Conjured_Backstage_Pass_Increase_By_2_Quality_When_SellIn_More_Than_10()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Backstage passes to a TAFKAL80ETC concert");
+        np.Items[0].SellIn.Should().Be(14);
+        np.Items[0].Quality.Should().Be(22);
+    }
+
+    [Fact]
+    public void Backstage_Pass_Increase_By_2_Quality_When_SellIn_Less_Than_10_Days()
     {
         // Arrange
         var np = new Program();
@@ -140,7 +241,24 @@ public class ProgramTests
     }
 
     [Fact]
-    public void Backstage_Pass_Increase_By_3_When_SellIn_Less_Than_5_Days()
+    public void Conjured_Backstage_Pass_Increase_By_4_Quality_When_SellIn_Less_Than_10_Days()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Backstage passes to a TAFKAL80ETC concert", 9, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Backstage passes to a TAFKAL80ETC concert");
+        np.Items[0].SellIn.Should().Be(8);
+        np.Items[0].Quality.Should().Be(24);
+    }
+
+    [Fact]
+    public void Backstage_Pass_Increase_By_3_Quality_When_SellIn_Less_Than_5_Days()
     {
         // Arrange
         var np = new Program();
@@ -154,6 +272,23 @@ public class ProgramTests
         np.Items[0].Name.Should().Be("Backstage passes to a TAFKAL80ETC concert");
         np.Items[0].SellIn.Should().Be(3);
         np.Items[0].Quality.Should().Be(23);
+    }
+
+    [Fact]
+    public void Conjured_Backstage_Pass_Increase_By_6_Quality_When_SellIn_Less_Than_5_Days()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Backstage passes to a TAFKAL80ETC concert", 4, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Backstage passes to a TAFKAL80ETC concert");
+        np.Items[0].SellIn.Should().Be(3);
+        np.Items[0].Quality.Should().Be(26);
     }
     
     [Fact]
@@ -174,6 +309,23 @@ public class ProgramTests
     }
 
     [Fact]
+    public void Conjured_Backstage_Pass_Quality_0_When_Concert_Done()
+    {
+        // Arrange
+        var np = new Program();
+        var item = np.CreateItem("Conjured Backstage passes to a TAFKAL80ETC concert", 0, 20);
+        np.Items = new List<Item> { item };
+
+        // Act
+        np.UpdateItems();
+
+        // Assert
+        np.Items[0].Name.Should().Be("Conjured Backstage passes to a TAFKAL80ETC concert");
+        np.Items[0].SellIn.Should().Be(-1);
+        np.Items[0].Quality.Should().Be(0);
+    }
+
+    [Fact]
     public void Conjured_Item_Degrades_By_2_Quality_When_Not_Expired()
     {
         // Arrange
@@ -188,23 +340,6 @@ public class ProgramTests
         np.Items[0].Name.Should().Be("Conjured Mana Cake");
         np.Items[0].SellIn.Should().Be(9);
         np.Items[0].Quality.Should().Be(18);
-    }
-
-    [Fact]
-    public void Conjured_Item_Degrades_By_4_Quality_When_Expired()
-    {
-        // Arrange
-        var np = new Program();
-        var item = np.CreateItem("Conjured Mana Cake", 0, 20);
-        np.Items = new List<Item> { item };
-
-        // Act
-        np.UpdateItems();
-
-        // Assert
-        np.Items[0].Name.Should().Be("Conjured Mana Cake");
-        np.Items[0].SellIn.Should().Be(-1);
-        np.Items[0].Quality.Should().Be(16);
     }
 
     [Fact]
