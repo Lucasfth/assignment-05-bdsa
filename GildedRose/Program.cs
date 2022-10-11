9c621c775fdaee7eef2728c681c8e524afc7109d
Lucasfth <luha@itu.dk>
2022-10-11 10:54:17 +0200

    change check in CreateItem to oneliner

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -56,8 +56,7 @@ public class Program
         else
             item = new Normal() { Name = name, SellIn = sellIn, Quality = quality };
         
-        if (nameLower.Contains("conjured"))
-            item.IsConjured();
+        if (nameLower.Contains("conjured")) item.IsConjured();
         
         return item;
     }
